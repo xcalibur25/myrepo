d74746072178915825caf34c575a6026fea62f51
Yash <yashraorane25@gmail.com>
2019-01-20 13:38:47 +0530

    Hello World

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello Yash Raorane!!");
+        int myFirstNumber= (5+10)+(2*10);
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+
+        int mySecindNumber = 6;
+        int myThirdNumber = 2 * myFirstNumber;
+        int myTotal= myFirstNumber + mySecindNumber + myThirdNumber;
+        System.out.println(myTotal );
+    }
+}
